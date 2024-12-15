5fe0c7f550a3d04f8f573af701d906e6cfcd65a4
WorkerHarder171 <dadybima171@gmail.com>
2024-12-15 19:03:20 +0700

    update: smoothing profile and give nice ui at notes

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,8 @@
+import { defineConfig, UserConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vite.dev/config/
 const config: UserConfig = defineConfig({
   plugins: [react()],
   resolve: {
